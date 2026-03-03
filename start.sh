094a64aaa010e20314f3058574994a761169ee1e
Nagy Zétény <nagyzeteny6@gmail.com>
2026-03-03 18:55:49 +0100

    feat: use docker to install deps

--- start.sh
+++ start.sh
@@ -14,7 +14,12 @@ if ! docker volume inspect shared_composer >/dev/null 2>&1; then
   docker volume create shared_composer
 fi
 
-cd frontend && pnpm install
+docker run --rm \
+  -v "$(pwd)/frontend:/app" \
+  -v shared_pnpm:/shared_pnpm \
+  -w /app \
+  node:24-alpine \
+  sh -c "corepack enable && pnpm install --store-dir /shared_pnpm --dangerously-allow-all-builds"
 
 docker compose up -d
 
