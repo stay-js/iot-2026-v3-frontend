dfcd5061915d82dff13a9f808bc8a7df973fd940
Nagy Zétény <nagyzeteny6@gmail.com>
2026-03-03 16:50:12 +0100

    feat: remove select cursor reset

--- frontend/src/components/ui/select.tsx
+++ frontend/src/components/ui/select.tsx
@@ -57,7 +57,7 @@ function SelectItem({
   return (
     <SelectPrimitive.Item
       className={cn(
-        "ata-disabled:opacity-50 relative flex w-full cursor-default items-center gap-2 rounded-sm py-1.5 pr-8 pl-2 text-sm outline-hidden select-none focus:bg-accent focus:text-accent-foreground data-disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 [&_svg:not([class*='text-'])]:text-muted-foreground *:[span]:last:flex *:[span]:last:items-center *:[span]:last:gap-2",
+        "ata-disabled:opacity-50 relative flex w-full cursor-pointer items-center gap-2 rounded-sm py-1.5 pr-8 pl-2 text-sm outline-hidden select-none focus:bg-accent focus:text-accent-foreground data-disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 [&_svg:not([class*='text-'])]:text-muted-foreground *:[span]:last:flex *:[span]:last:items-center *:[span]:last:gap-2",
         className,
       )}
       data-slot="select-item"
